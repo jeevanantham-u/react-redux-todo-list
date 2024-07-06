7d1ef921cd35bb3381504e2d05b2e4ea6b693cc3
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-07-06 23:18:48 +0530

    redux store ceated

--- src/components/UpdateModel.jsx
+++ src/components/UpdateModel.jsx
@@ -1,9 +1,17 @@
-import AddTask from './AddTask';
+import { useState } from 'react';
+
+import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 
 
 export const UpdateModel = (props) => {
+    const [task, setTask] = useState({
+
+    });
+    function handleChange() {
+        console.log("jeeva");
+    }
     return (
         <>
             <Modal
@@ -18,7 +26,21 @@ export const UpdateModel = (props) => {
                     </Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <AddTask />
+                    <Form>
+                        <Form.Group className="mb-3" controlId="formBasicEmail">
+                            <Form.Label>Edit title</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Title" name="title"
+                                value={task.title}
+                                onChange={handleChange}
+                            />
+                        </Form.Group>
+
+                        <Form.Group className="mb-3" controlId="formBasicPassword">
+                            <Form.Label>Edit task</Form.Label>
+                            <Form.Control type="text" placeholder="Enter Task" name="task" value={task.task}
+                                onChange={handleChange} />
+                        </Form.Group>
+                    </Form>
                 </Modal.Body>
                 <Modal.Footer>
                     <Button onClick={props.onHide}>Close</Button>
--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,18 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.jsx'
+
 import './index.css'
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
-import "bootstrap-icons/font/bootstrap-icons.css"; 
+import "bootstrap-icons/font/bootstrap-icons.css";
+
+import { Provider } from 'react-redux';
+import store from '../store/store.js';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <Provider store={store} >
+      <App />
+    </Provider>
   </React.StrictMode>,
 )
