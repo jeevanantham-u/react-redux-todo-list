f347a2f9b6b0749908d1c66dff07f6fe2f331b19
jeevanantham-u <jeevanantham2k2@gmail.com>
2024-07-11 23:37:46 +0530

    task count and server created

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,11 @@
-import React from 'react'
+import { useSelector } from "react-redux";
 
 const Header = () => {
+  const {tasksList } =useSelector(state => state.tasks)
   return (
     <>
         <h1 className=' text-center my-4 text-primary'>Project management</h1>
-        <p className='text-center lead '>Currently 1 task(s) pending.</p>
+        <p className='text-center lead '> Currently { tasksList.length } task(s) pending.</p>
     </>
   )
 }
